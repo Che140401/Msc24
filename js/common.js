78e3fd618af334bc02c6c2b1752ca8d0ab33556a
afzal hamdulay <afzal.hamdulay@theadroit.in>
2023-11-08 18:44:58 +0530

    added a whole lot of css and sections

--- js/common.js
+++ js/common.js
@@ -1,8 +1,99 @@
-$('.courses-box .left-box').slick({
-    vertical: true,  
+$(document).ready(function () {
+
+  $('.banner-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+  });
+
+  $(document).on('click', '[data-tab]', function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    var dataTab = $(this).attr('data-tab');
+    $('#' + dataTab).siblings().removeClass("active");
+    $('#' + dataTab).addClass('active');
+  });
+
+  $('.courses-box .left-box').slick({
+    vertical: true,
     verticalSwiping: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows:false,
-    dots:true
+    arrows: false,
+    dots: true
   });
+
+  const scrollContain = document.querySelector(".scroll-images .scroll-contain");
+  const images = scrollContain.querySelectorAll("img");
+
+  for (const image of images) {
+    const copyImage = image.cloneNode(true);
+    scrollContain.appendChild(copyImage);
+  }
+
+  $(function ($, win) {
+    $.fn.inViewport = function (cb) {
+      return this.each(function (i, el) {
+        function visPx() {
+          var H = $(this).height(),
+            r = el.getBoundingClientRect(),
+            t = r.top,
+            b = r.bottom;
+          return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
+        }
+        visPx();
+        $(win).on("resize scroll", visPx);
+      });
+    };
+  }(jQuery, window));
+
+  jQuery(function ($) {
+    $(".counter").inViewport(function (px) {
+      if (px > 0 && !this.initNumAnim) {
+        this.initNumAnim = true;
+        $(this).prop('Counter', 0).animate({
+          Counter: $(this).text()
+        }, {
+          duration: 1000,
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      }
+    });
+  });
+
+  $(document).click(function () {
+    $(".close-enq").trigger("click");
+  });
+
+  $('.left-enq-form').click(function (e) {
+    e.stopPropagation();
+  });
+
+  $(document).on("click", ".register-now", function (e) {
+    $(".left-enq-form").css("width", "30.0rem");
+    e.stopPropagation();
+  });
+
+  $(".close-enq").click(function () {
+    $(".left-enq-form").css("width", "0");
+  });
+
+  $(".next-btn").click(function () {
+    $(".shedule-inner-wrap").toggleClass("active");
+  });
+
+  if ($(window).width() <= 1023) {
+    $(document).on("click", ".register-now", function (e) {
+      $(".left-enq-form").css("width", "40.0rem");
+      e.stopPropagation();
+    });
+
+    $(".close-enq").click(function () {
+      $(".left-enq-form").css("width", "0");
+    });
+  }
+
+});
