ff506e32059ff09a029172be50792cb04e51000f
Vinesh <vinesh.gurram@theadroit.in>
2023-11-08 17:27:58 +0530

    vinesh code

--- js/common.js
+++ js/common.js
@@ -1,8 +1,28 @@
-$('.courses-box .left-box').slick({
-    vertical: true,  
-    verticalSwiping: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows:false,
-    dots:true
+  $( document ).ready(function() {
+    $('.courses-box .left-box').slick({
+      vertical: true,  
+      verticalSwiping: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows:false,
+      dots:true
+    });
+    
+    $('.box-wrappper-slider').slick({
+      infinite:true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows:true,
+      dots:false,
+      autoplay:true
+    });
+
+    $(".prev").click(function(){
+      $(".section-what-clients-say .slick-prev").trigger("click")
+    })
+
+    $(".next").click(function(){
+      $(".section-what-clients-say .slick-next").trigger("click")
+    })
+
   });
